4baa8936d2a5fe6827f101e8c59abff967932bbc
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-07 11:28:32 +0530

    Update default.sh

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,3 +1,9 @@
+## Install the required packages mentioned in config-mgmt-tool/package_list/packages file 
 bash install.sh
+
+## Configure the files and restart services mentioned 
 bash configure.sh
+
+
+## Cleanup if required
 bash cleanup.sh
