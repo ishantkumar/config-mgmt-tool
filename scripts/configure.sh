1d19f77b03137f72c5ace8b2030e24e5d19ebb7d
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-09 12:39:12 +0530

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,5 +1,9 @@
-## to start
+## copy files
 rm -f /var/www/html/index.html
 cp -nrf ../files/php_app.php /var/www/html/index.php
 chown www-data:root /var/www/html/index.php
 chmod 644 /var/www/html/index.php
+
+
+## restarting services
+service apache2 restart
