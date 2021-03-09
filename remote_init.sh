681e52f87377d517931560aaa395da692f9b22f3
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-09 17:42:18 +0530

    Update remote_init.sh

--- remote_init.sh
+++ remote_init.sh
@@ -6,17 +6,17 @@ set password [lindex $argv 1]
 set install_package [lindex $argv 2]
 
 
-spawn scp -o StrictHostKeyChecking=no remote_init.sh root@$server_name:/root/
+spawn scp -o StrictHostKeyChecking=no init.sh root@$server_name:/root/
 expect "password"
 send "$password\r"
 interact
 
-spawn ssh -o StrictHostKeyChecking=no root@$server_name chmod +x /root/remote_init.sh
+spawn ssh -o StrictHostKeyChecking=no root@$server_name chmod +x /root/init.sh
 expect "password"
 send "$password\r"
 interact
 
-spawn ssh -o StrictHostKeyChecking=no root@$server_name /root/remote_init.sh -i $install_package
+spawn ssh -o StrictHostKeyChecking=no root@$server_name /root/init.sh -i $install_package
 expect "password"
 send "$password\r"
 interact
