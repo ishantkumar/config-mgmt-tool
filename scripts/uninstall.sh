793e0a0c9420dddc1aae44ee1eeef2de14f2b1d1
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-09 08:57:37 +0530

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-package_type=$1
+uninstall_package_type=$1
 while IFS= read -r line; do
     apt remove $line -y
-done < ../package_list/$package_type
+done < ../package_list/$uninstall_package_type
