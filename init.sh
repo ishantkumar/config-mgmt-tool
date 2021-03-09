165ad90126f9a464beb5b9be9bdad2ff37b6be57
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-09 17:41:30 +0530

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while getopts i:u: flag
+do
+    case "${flag}" in
+        i) install=${OPTARG};;
+        u) uninstall=${OPTARG};;
+        *) echo "please see the usage \n use -i for install_package and -u to uninstall package";;
+    esac
+done
+
+
+
+
+git clone https://github.com/ishantkumar/config-mgmt-tool.git
+cd config-mgmt-tool/scripts
+
+## Parameters
+package_type=$install
+uninstall_package_type=$uninstall
+
+## Install the required packages mentioned in config-mgmt-tool/package_list/packages file 
+bash install.sh $package_type
+
+## Configure the files and restart services mentioned 
+bash configure.sh
+
+
+## UnInstall the required packages mentioned in config-mgmt-tool/package_list/packages file 
+bash uninstall.sh $uninstall_package_type
+
+## Cleanup if required
+bash cleanup.sh
