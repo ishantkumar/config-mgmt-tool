bfa3f6b358123faf3280b90053018d16a0739cff
ishantkumar <mr.ishantkumar@gmail.com>
2021-03-09 14:56:08 +0530

    Update remote_init.sh

--- remote_init.sh
+++ remote_init.sh
@@ -1,3 +1,5 @@
+php_app_package=$1
+java_app_package=$2
 git clone https://github.com/ishantkumar/config-mgmt-tool.git
 cd config-mgmt-tool/scripts
-bash default.sh -i php_app_package -u java_app_package
+bash default.sh -i $php_app_package -u $java_app_package
